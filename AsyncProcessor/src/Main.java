7ed7013902e59e7d0a5f0edaf8f704dde863ab47
Abhishek Chikane <abhishek.chikane@gmail.com>
2012-10-24 14:35:58 +0530

    Adding Main class.

---
+++ AsyncProcessor/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Abhishek Chikane
+ *
+ */
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Async Processor");
+	}
+}
